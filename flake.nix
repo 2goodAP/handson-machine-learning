3b2c51f84580804a7ea2979a57b716fb7c665e87
2goodAP <45424904+2goodAP@users.noreply.github.com>
2025-03-19 15:04:46 +0545

    Fix `MAMBA_ROOT_PREFIX` path in flake.nix

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
             profile = ''
               set -e
               eval "$(micromamba shell hook --shell=posix)"
-              export MAMBA_ROOT_PREFIX=${builtins.getEnv "PWD"}/.micromamba
+              export MAMBA_ROOT_PREFIX="$(realpath './.micromamba')"
               export CONDA_OVERRIDE_CUDA="11.8"
 
               env="homl"
